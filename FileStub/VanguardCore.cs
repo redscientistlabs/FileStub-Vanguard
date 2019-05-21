5e6c5196c5a6dccd5659e9b98ba6607f6268f3b4
Phil Girard <phil@redscientist.com>
2019-05-20 22:46:05 -0400

    Re-enabled multithread support

--- FileStub/VanguardCore.cs
+++ FileStub/VanguardCore.cs
@@ -138,7 +138,7 @@ namespace Vanguard
             partial[VSPEC.SUPPORTS_KILLSWITCH] = false;
             partial[VSPEC.SUPPORTS_MIXED_STOCKPILE] = false;
             partial[VSPEC.REPLACE_MANUALBLAST_WITH_GHCORRUPT] = true;
-            //partial[VSPEC.SUPPORTS_MULTITHREAD] = true;
+            partial[VSPEC.SUPPORTS_MULTITHREAD] = true;
             //partial[VSPEC.CONFIG_PATHS] = new[] { Path.Combine(emuDir, "config.ini") };
 
             return partial;
